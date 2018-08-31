aa1b5e7a3b36870b0989106520dd663d0fd06960
Thomas Pope <tpope0928@gmail.com>
2018-08-31 15:03:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,15 +22,18 @@ class Song
     @@genres.uniq
   end
 
-  def self.genre_count
+ def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count [genre]
-        genre_count[genre] += 1
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
       end
     end
     genre_count
   end
+end
   
   def self.artists
     @@artists.uniq
