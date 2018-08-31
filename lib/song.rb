65d65b000751eeaf284a9e02467048e80a0f1a55
Thomas Pope <tpope0928@gmail.com>
2018-08-31 15:02:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count [genre]
-        genre_count[genre] + 1
+        genre_count[genre] += 1
       end
     end
     genre_count
