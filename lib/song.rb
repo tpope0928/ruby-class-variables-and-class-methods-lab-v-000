d58da3b4d079c7f6ac8099e6c4155aefd08f7db4
Thomas Pope <tpope0928@gmail.com>
2018-08-31 15:00:25 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@song_count = 0
+  @@genres = []
+  @@artist = []
+  
+  def initialize
+    @name = name
+    @genre = genre
+    @artist = artist
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@song_count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count [genre]
+        
+  end
+
+end
